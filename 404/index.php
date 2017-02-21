5f9fae5d8db11d0640b2400258d78e077e28c2b5
Gogs <kyle@kylefrost.me>
2017-02-21 18:27:15 -0500

    slight wording change

--- 404/index.php
+++ 404/index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <div class="wrap">
-            <div style="font-size: 20pt;">You've fallen in foul territory.</div>
+            <div style="font-size: 20pt;">You've landed in foul territory.</div>
             <a id="change_team_404" href="/change_team"><i>Click here to select team</i></a>
         </div>
         <script src="/js/google.js"></script>
